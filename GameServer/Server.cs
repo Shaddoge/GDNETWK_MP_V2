9e6c62084ea496b738f41bed6a5c0d160ecbab6f
John Santos <john_adriel_santos@dlsu.edu.ph>
2022-06-17 03:39:20 +0800

    Added packet handlers, thread manager.

--- GameServer/Server.cs
+++ GameServer/Server.cs
@@ -9,9 +9,11 @@ namespace GameServer
     class Server
     {
         public static int MaxPlayers { get; private set; }
-        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
         public static int Port { get; private set; }
-        
+        public static Dictionary<int, Client> clients = new Dictionary<int, Client>();
+        public delegate void PacketHandler(int _fromClient, Packet _packet);
+        public static Dictionary<int, PacketHandler> packetHandlers;
+
         private static TcpListener tcpListener;
 
         public static void Start(int _maxPlayers, int _port)
@@ -53,6 +55,12 @@ namespace GameServer
             {
                 clients.Add(i, new Client(i));
             }
+
+            packetHandlers = new Dictionary<int, PacketHandler>()
+            {
+                { (int)ClientPackets.welcomeReceived, ServerHandle.WelcomeReceived }
+            };
+            Console.WriteLine("Initialized packets.");
         }
     }
 }
