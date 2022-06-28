1abe306e9ea0540f1dc8cf1c1fb0883513a88153
John Santos <john_adriel_santos@dlsu.edu.ph>
2022-06-28 21:40:15 +0800

    ProfileManager should be fixed. Changed sorting order of main canvas

--- Unity Client/Assets/Scripts/Managers/ProfileManager.cs
+++ Unity Client/Assets/Scripts/Managers/ProfileManager.cs
@@ -37,12 +37,19 @@ public class ProfileManager : MonoBehaviour
 
     public void TimerStarted()
     {
-        PlayerProfileList[Client.instance.myId - 1].panel.transform.GetChild(1).GetComponent<Toggle>().interactable = false;
+        int profileIndex = GetProfileIndex(Client.instance.myId);
+
+        if (profileIndex == -1) return;
+        PlayerProfileList[profileIndex].panel.transform.GetChild(1).GetComponent<Toggle>().interactable = false;
     }
 
     public void ToggleCheck(int _id, bool _isReady)
     {
-        PlayerProfileList[_id - 1].panel.transform.GetChild(1).gameObject.SetActive(_isReady);
+        int profileIndex = GetProfileIndex(Client.instance.myId);
+
+        if (profileIndex == -1) return;
+
+        PlayerProfileList[profileIndex].panel.transform.GetChild(1).gameObject.SetActive(_isReady);
     }
 
     public void ResetAll()
@@ -94,13 +101,23 @@ public class ProfileManager : MonoBehaviour
 
     public void RemoveProfile(int _id)
     {
+        int indexToRemove = GetProfileIndex(_id);
+
+        if (indexToRemove == -1) return;
+
+        Destroy(PlayerProfileList[indexToRemove].panel);
+        PlayerProfileList.RemoveAt(indexToRemove);
+    }
+
+    private int GetProfileIndex(int _id)
+    {
         for (int i = 0; i < PlayerProfileList.Count; i++)
         {
             if (PlayerProfileList[i].id == _id)
             {
-                Destroy(PlayerProfileList[i].panel);
-                PlayerProfileList.RemoveAt(i);
+                return i;
             }
         }
+        return -1;
     }
 }
