f7a355582f11b7ab6bbc592aa57f04c922f40f7d
John Santos <john_adriel_santos@dlsu.edu.ph>
2022-06-30 00:29:11 +0800

    Fixed billboard text.

--- Unity Client/Assets/Scripts/UI/Billboard.cs
+++ Unity Client/Assets/Scripts/UI/Billboard.cs
@@ -10,14 +10,21 @@ public class Billboard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mainCamera = Camera.main;
+        mainCamera = Camera.current;
         originalRotation = transform.rotation;
     }
 
     // Update is called once per frame
-    private void Update()
+    private void LateUpdate()
     {
+        if (mainCamera != Camera.current)
+            mainCamera = Camera.current;
+
+        if (mainCamera == null) return;
         transform.LookAt(2 * transform.position - mainCamera.transform.position);
+
+        //transform.LookAt(mainCamera.transform);
+        transform.rotation = Quaternion.Euler(0f, transform.rotation.eulerAngles.y, 0f);
         //transform.rotation = mainCamera.transform.rotation * originalRotation;
     }
 }
